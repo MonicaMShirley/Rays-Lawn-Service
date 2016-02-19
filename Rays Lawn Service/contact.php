471215168b344234d0f081cf78f2c158e57b97a4
Monica Shirley <monicamshirley@gmail.com>
2016-02-19 18:57:42 -0500

    Ray's lawn service-Master
    First upload of Ray's Lawn Service Files

---
+++ Rays Lawn Service/contact.php
@@ -0,0 +1,178 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="Monica M. Shirley" content="">
+
+    <title>Contact - Ray's Lawn Service</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/rayslawnservice.css" rel="stylesheet">
+
+ 
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Ray's Lawn Service</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="about.html">About Us</a>
+                    </li>
+                    <li>
+                        <a href="services.html">Services</a>
+                    </li>
+                    <li>
+                        <a href="gallery.html">Gallery</a>
+                    </li>
+                    <li>
+                        <a href="contact.php">Contact for a Free Estimate!</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <hr>
+                           <h2 class="intro-text text-center">Contact Us!</h2>
+                        <hr>
+                    </div>
+                </div>
+
+            
+                 <div class="col-md-12 main-content" id="contactForm"><br>           
+                <div class="col-md-6 text-center">
+                        <h3><strong>Ray's Lawn Service</strong></h3>
+                        <p>Phone: 336.985.3818</p>
+                        <p>Address: 609 N. Main St<br>King, NC 27021</p>
+                        <p>Email: <a href=""></a></p>
+                </div>
+                
+                <div class="col-md-6 text-center">
+                        <h3><strong>Service Areas:</strong></h3>
+                        <p>We service King, <br>Rural Hall, Winston Salem, <br>and surrounding areas.</p>
+                </div>
+    <div class="clearfix"></div>
+            
+                <h2 class="text-center">Contact Form</h2>
+                
+                    <p class="text-center">If you are interested in service, or have any questions, please give us a call or send 
+                        us a message in the form provided below.</p>
+                                        
+                <br>
+                     
+                    <form method="post">
+                        <div class="row">
+                            <div class="form-group col-lg-4">
+                                <label for="name">Name</label>
+                                    <input name="name" value="" type="text" class="form-control">
+                            </div>
+                            <div class="form-group col-lg-4">
+                                <label for="email">Email Address</label>
+                                    <input name="email" value="" type="email" class="form-control">
+                            </div>
+                            
+                            <div class="form-group col-lg-4">
+                                <label for="phone">Phone Number</label>
+                                    <input name="phone" value="" type="number" class="form-control">
+                            </div>
+                            
+                            <div class="clearfix"></div>
+                            
+                            <div class="form-group col-lg-12">
+                                <label for="address">Street Address</label>
+                                    <input name="address" value="" type="text" class="form-control">
+                            </div>
+                            
+                            <div class="form-group col-lg-12">
+                                <label for="message">Message</label>
+                                    <textarea name="message" class="form-control" rows="4"></textarea>
+                            </div>
+
+                            <div class="form-group col-lg-12">
+                                <input type="submit" name="submit" class="btn btn-success btn-lg" value="Submit">
+                            </div>
+                        </div>
+                    </form>
+                                    
+                 
+            </div>
+
+        </div>
+            
+            
+        
+                            
+                                    
+    <!-- /.container -->
+<hr style="width:75%;">
+    <footer>
+
+        <footer>
+            <div class="row">
+                <div class="box">
+                    <div class="col-lg-12 text-center">
+                        <a href="index.html">Home</a> |
+
+                        <a href="about.html">About Us</a> |
+                    
+                        <a href="services.html">Services</a> |
+                    
+                        <a href="gallery.html">Gallery</a> |
+                    
+                        <a href="contact.php">Contact Us!</a>
+                    
+                        <p>Copyright &copy; Ray's Lawn Services Inc.
+                        <br>
+                        By: Monica M. Shirley</p>
+                    </div><!-- /.col-lg-12 -->
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+</div>
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
